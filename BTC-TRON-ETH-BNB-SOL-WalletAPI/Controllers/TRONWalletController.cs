b326001e22bd4e7dc2d194f8ac0582278bbb35f6
Kubilay <145432835+kubilay13@users.noreply.github.com>
2025-08-06 16:25:57 +0300

    Add project files.

---
+++ BTC-TRON-ETH-BNB-SOL-WalletAPI/Controllers/TRONWalletController.cs
@@ -0,0 +1,43 @@
+﻿using BTC_TRON_ETH_BNB_SOL_WalletAPI.Serivces.WalletServices;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace BTC_TRON_ETH_BNB_SOL_WalletAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TRONWalletController : ControllerBase
+    {
+        private readonly TronWalletServices _tronWalletServices;
+
+        public TRONWalletController(TronWalletServices tronWalletServices)
+        {
+            _tronWalletServices = tronWalletServices;
+        }
+
+        [HttpGet("TronWallet")]
+        public async Task<IActionResult> GetTronWallet()
+        {
+            try
+            {
+                var result = await _tronWalletServices.GetTronWalletAsync();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpGet("download-wallet")]
+        public async Task<IActionResult> DownloadWallet()
+        {
+            var content = await _tronWalletServices.GetTronWalletAsync();
+
+            var bytes = Encoding.UTF8.GetBytes(content);
+            var stream = new MemoryStream(bytes);
+
+            return File(stream, "text/plain", "tron_wallet.txt"); 
+        }
+    }
+}
---
+++ BTC-TRON-ETH-BNB-SOL-WalletAPI/Serivces/WalletServices/TronWalletServices.cs
@@ -0,0 +1,19 @@
+﻿using Solnet.Wallet;
+using TronNet;
+
+namespace BTC_TRON_ETH_BNB_SOL_WalletAPI.Serivces.WalletServices
+{
+    public class TronWalletServices
+    {
+        public async Task<string> GetTronWalletAsync()
+        {
+           var key =TronECKey.GenerateKey(TronNetwork.MainNet);
+           var address = key.GetPublicAddress();
+           var privatekey = key.GetPrivateKey();
+
+            return $"Address: {address}{Environment.NewLine}Private Key: {privatekey}";
+
+        }
+
+    }
+}
